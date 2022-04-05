e2b362e5e87a7942d0ae7223da12a85b2c617d47
DrkNowak <dariusznowak194@gmail.com>
2022-04-05 19:30:46 +0200

    feat: user Credentials p1

---
+++ src/components/Loan/LoanCredentials.vue
@@ -0,0 +1,36 @@
+<template>
+  <p>CREDENTIALS</p>
+  <div v-for="field in loanCredentialsFields" :key="field">
+      <span>{{ field }}</span>
+      <input ref="input" @change="handleChange($event, field)"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+import loanCredentialsFields from '@/consts/loanCredentialsFields'
+
+export default defineComponent({
+  emits: ['change'],
+
+  data () {
+    return {
+      loanCredentialsFields
+    }
+  },
+
+  methods: {
+    handleChange (event: { target: HTMLInputElement }, field: string): void {
+      this.$emit('change', event.target.value, field)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+p {
+  margin-top:20px;
+  font-size: 50px;
+}
+</style>
---
+++ src/consts/loanCredentialsFields.js
@@ -0,0 +1,1 @@
+export default ['name', 'surname', 'dateOfBirth', 'adress', 'satisfy']
